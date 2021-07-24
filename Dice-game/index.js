27148b1baa51abc51618c693efbb04e4cffb5b84
Anganam <48564923+Anganam@users.noreply.github.com>
2021-07-25 00:05:35 +0200

    Update index.js

--- Dice-game/index.js
+++ Dice-game/index.js
@@ -1,18 +1,22 @@
+// Generate random number from 1 to six
 var randomNumber1 =  Math.floor(Math.random() * 6 + 1);
 var randomNumber2 =  Math.floor(Math.random() * 6 + 1);
 
+// Concatenate the random nubmer with the prefix and suffix of the image URI
 var player1 = "images/dice"+randomNumber1+".png";
 var player2 = "images/dice"+randomNumber2+".png";
 
-document.getElementsByClassName("img1")[0].setAttribute("src",player1);
-document.getElementsByClassName("img2")[0].setAttribute("src",player2);
+// Dynamically change the image source URI
+document.querySelectorAll("img")[0].setAttribute("src",player1);
+document.querySelectorAll("img")[1].setAttribute("src",player2);
 
+// logic to decide the the winner
 if (randomNumber1 === randomNumber2){
     document.querySelector("h1").textContent = "Draw";
 } 
 else if ( randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "Player 1 Wins"
+    document.querySelector("h1").textContent = "🚩Player 1 Wins";
 }else
 {
-    document.querySelector("h1").textContent = "Player 2 Wins"
+    document.querySelector("h1").textContent = "Player 2 Wins🚩";
 }
